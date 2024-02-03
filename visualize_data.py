6675d086146c66a2658df7c63777788ea040d2bc
DavidC001 <davidecavicchini@hotmail.com>
2024-02-03 11:08:56 +0100

    document script

--- visualize_data.py
+++ visualize_data.py
@@ -14,7 +14,9 @@ from torchvision.io import read_image
 import matplotlib.pyplot as plt
 import torchvision.transforms as T
 
-
+'''
+Define the SimCLR base model with encoder and projection head
+'''
 class MLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, out_dim):
         super().__init__()
@@ -31,15 +33,24 @@ from torchvision.models import resnet50, ResNet50_Weights
 
 
 def get_simclr_net():
+    """
+    Returns the SimCLR network model.
+
+    This function creates a SimCLR network model by using a pre-trained ResNet50 model
+    as the backbone and replacing the fully connected layer with a custom MLP layer.
+
+    Returns:
+        model (nn.Module): SimCLR network model.
+    """
     weights = ResNet50_Weights.DEFAULT
     model = resnet50(weights=weights)
     model.fc = MLP(2048, 2048, 128)
 
     return model
 
-from flash.core.optimizers import LARS
-
-
+'''
+Define the dataloader for the clustering task
+'''
 class ClusterDataset(Dataset):
     def __init__(self, transform, data_set='training'):
 
@@ -73,6 +84,16 @@ class ClusterDataset(Dataset):
         return sample
 
 def get_cluster_data(batch_size):
+    """
+    Get the cluster dataset and data loader.
+
+    Args:
+        batch_size (int): The batch size for the data loader.
+
+    Returns:
+        cluster_dataset (ClusterDataset): The cluster dataset.
+        cluster_loader (DataLoader): The data loader for the cluster dataset.
+    """
     transforms = T.Compose(
         [
             T.ToTensor(),
@@ -85,7 +106,21 @@ def get_cluster_data(batch_size):
 
     return cluster_dataset, cluster_loader
 
+
 def extract_representations(path, cluster_loader, load=True):
+    """
+    Extracts representations from images using a SimCLR network.
+
+    Args:
+        path (str): The path to the saved model checkpoint.
+        cluster_loader (torch.utils.data.DataLoader): The data loader for the images.
+        load (bool, optional): Whether to load the model weights from the checkpoint. 
+            Defaults to True.
+
+    Returns:
+        numpy.ndarray: The concatenated base representations.
+        numpy.ndarray: The concatenated projected representations.
+    """
     net = get_simclr_net()
 
     if load:
@@ -111,6 +146,16 @@ def extract_representations(path, cluster_loader, load=True):
 from sklearn.cluster import KMeans
 
 def kmeans_algorithm(features, n_clusters=8):
+    """
+    Applies the K-means algorithm to the given features.
+
+    Parameters:
+        features (array-like): The input features to be clustered.
+        n_clusters (int): The number of clusters to create (default is 8).
+
+    Returns:
+        array-like: The cluster labels assigned to each data point.
+    """
     kmeans = KMeans(n_clusters=n_clusters)
     kmeans.fit(features)
 
@@ -121,12 +166,34 @@ def kmeans_algorithm(features, n_clusters=8):
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 def reduce_dim(features, labels):
+    """
+    Reduce the dimensionality of the input features using Linear Discriminant Analysis (LDA).
+
+    Parameters:
+        features (array-like): The input features.
+        labels (array-like): The corresponding labels for the features.
+
+    Returns:
+        array-like: The transformed features with reduced dimensionality.
+    """
     lda = LDA(n_components=3)
     lda.fit(features, labels)
 
     return lda.transform(features)
 
 def plot_clusters(dataset, clusters, features, title):
+    """
+    Plots clusters in a 3D scatter plot and displays corresponding images on selection.
+
+    Parameters:
+    - dataset (Dataset): The dataset containing the image paths.
+    - clusters (list): List of cluster labels for each data point.
+    - features (ndarray): Array of features for each data point.
+    - title (str): Title of the plot.
+
+    Returns:
+    None
+    """
     colors = {
         0: '#F8512E', 1: '#F8F82E',
         2: '#40F82E', 3: '#2EC1F8',
@@ -163,6 +230,23 @@ from sklearn.metrics import silhouette_score
 
 
 def cluster(model_path, load=True):
+    """
+    Clusters the data using the given model and returns the cluster set, base features, projected features,
+    labels for base features, labels for projected features, reduced base features, and reduced projected features.
+
+    Parameters:
+    - model_path (str): The path to the model file.
+    - load (bool): Whether to load the model or not. Defaults to True.
+
+    Returns:
+    - cluster_set: The cluster set.
+    - base_features: The base features.
+    - proj_features: The projected features.
+    - labels_base: The labels for base features.
+    - labels_proj: The labels for projected features.
+    - lda_base: The reduced base features.
+    - lda_proj: The reduced projected features.
+    """
     cluster_set, cluster_loader = get_cluster_data(30)
 
     base_features, proj_features = extract_representations(model_path, cluster_loader, load)
@@ -195,4 +279,7 @@ for file in os.listdir(path):
 
 path = path + 'simclr_epoch_{:d}.pth'.format(epoch)
 
+#path = 'trained_models/ver1.pt'
+
+#cluster the data
 cluster_set, base_features, proj_features, labels_base, labels_proj, pca_base, pca_proj = cluster(path)
