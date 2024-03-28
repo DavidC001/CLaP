0aa1c9de9ca0fbc8f98045a34ade5d81f509abad
DavidC001 <davidecavicchini@hotmail.com>
2024-03-28 11:46:33 +0100

    added configurable contrastive pretraining from json

--- contrastive_HPE.py
+++ contrastive_HPE.py
@@ -1,7 +1,38 @@
 import sys
 sys.path.append('.')
 
-from contrastive_training.train import contrastive_train
+import argparse
+import json
+import torch
 
-print("Test")
-contrastive_train(simclr=False, simsiam=False)
+from contrastive_training.train import contrastive_pretraining
+
+
+def main(args):
+    #read experiment json file
+    with open(args.experiment) as f:
+        data = json.load(f)
+    
+    #get the required parameters
+    device = data['device']
+    if device == None:
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    dataset = data['dataset']
+    if dataset == None:
+        dataset = 'panoptic'
+    models_dir = data['models_dir']
+    if models_dir == None:
+        models_dir = 'trained_models'
+
+    contrastive_pretraining(args=data["contrastive"], device=device, dataset=dataset, models_dir=models_dir)
+
+    
+
+
+if __name__ == "__main__":
+    #get experment file name from command line (required)
+    parser = argparse.ArgumentParser(description='Contrastive training')
+    parser.add_argument('--experiment', type=str, help='Path to the experiment json file', required=True)
+    args = parser.parse_args()
+    
+    main(args)
--- contrastive_training/train.py
+++ contrastive_training/train.py
@@ -5,23 +5,19 @@ from contrastive_training.simsiam.train import train_simsiam
 from contrastive_training.simclr.train import train_simclr
 
 
-def contrastive_train(device='cuda', dataset="panoptic",
-                      simsiam=True, name_siam = "simsiam",
-                      batch_size_siam=1024, lr_siam=0.01, wd_siam=0.000001, momentum_siam=0.9, t_siam=0.6, epochs_siam=100,
-                      simclr=True, name_clr = "simclr",
-                      batch_size_clr=1024, lr_clr=0.01, wd_clr=0.000001, momentum_clr=0.9, t_clr=0.6, epochs_clr=100
-                      ):
-    if simclr:
+def contrastive_train(simsiam, simclr, device='cuda', dataset="panoptic", models_dir="trained_models"):
+    if simclr["train"]:
         print("Training SimCLR")
         train_simclr(
-            name = name_clr, 
-            batch_size=batch_size_clr, 
-            device=device, 
-            learning_rate=lr_clr, 
-            weight_decay=wd_clr, 
-            momentum=momentum_clr, 
-            t=t_clr, 
-            epochs=epochs_clr,
+            model_dir=models_dir,
+            name = simclr["name"], 
+            batch_size=simclr["batch_size"],
+            device=device,
+            learning_rate=simclr["learning_rate"],
+            weight_decay=simclr["weight_decay"],
+            momentum=simclr["momentum"],
+            t=simclr["temperature"],
+            epochs=simclr["epochs"],
             dataset=dataset
         )
         print("SimCLR training done")
@@ -29,17 +25,60 @@ def contrastive_train(device='cuda', dataset="panoptic",
     if simsiam:
         print("Training SimSiam")
         train_simsiam(
-            name = name_siam, 
-            batch_size=batch_size_siam, 
-            device=device, 
-            learning_rate=lr_siam, 
-            weight_decay=wd_siam, 
-            momentum=momentum_siam, 
-            t=t_siam, 
-            epochs=epochs_siam,
+            model_dir=models_dir,
+            name = simsiam["name"],
+            batch_size=simsiam["batch_size"],
+            device=device,
+            learning_rate=simsiam["learning_rate"],
+            weight_decay=simsiam["weight_decay"],
+            momentum=simsiam["momentum"],
+            t=simsiam["temperature"],
+            epochs=simsiam["epochs"],
             dataset=dataset
         )
         print("SimSiam training done")
+
+def check_arguments_contrastive(args):
+    #name is required
+    assert 'name' in args, 'name not found for args'
+
+    if 'train' not in args:
+        args['train'] = True
+    if 'batch_size' not in args:
+        args['batch_size'] = 1024
+    if 'learning_rate' not in args:
+        args['learning_rate'] = 0.01
+    if 'weight_decay' not in args:
+        args['weight_decay'] = 0.000001
+    if 'momentum' not in args:
+        args['momentum'] = 0.9
+    if 'temperature' not in args:
+        args['temperature'] = 0.6
+    if 'epochs' not in args:
+        args['epochs'] = 100
+    
+    return args
+
+def contrastive_pretraining(args, device='cuda', dataset="panoptic", models_dir="trained_models"):
+    #skip if specified
+    if args['skip']:
+        print("Skipping contrastive training")
+        return
+    
+    print("Contrastive training")
+
+    simsiam = args['simsiam']
+    simclr = args['simclr']
+
+    #check if all the required parameters are present, if so use default values
+    simsiam = check_arguments_contrastive(simsiam)
+    simclr = check_arguments_contrastive(simclr)
+
+    contrastive_train(
+        device=device, dataset=dataset, models_dir=models_dir,
+        simsiam=simsiam,
+        simclr=simclr
+    )
 
 
 if __name__ == "__main__":
