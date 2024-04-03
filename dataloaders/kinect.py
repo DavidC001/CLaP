351121828dd5300407a03792c9f2dc806d7c8459
Alessia Pivotto <alessiapivotto1@gmail.com>
2024-04-03 14:11:38 +0200

    Adding kinect skeleton

--- dataloaders/kinect.py
+++ dataloaders/kinect.py
@@ -3,7 +3,6 @@ import torch
 from torch.utils.data import Dataset
 import cv2
 import random
-
 import matplotlib.pyplot as plt
 import torchvision.transforms as T
 
@@ -11,10 +10,33 @@ import torchvision.transforms as T
 class ContrastiveKinectDataset(Dataset):
     def __init__(self, transform, dataset_dir="datasets"):
         print("KinectDataset")
+        self.data_path = dataset_dir+"/KinectDataset/"
+        self.training_dir = []
+
+        self.transform = transform
+
+        paths = []
+
+        motion_seq = os.listdir(self.data_path)
+        no_dir = []
+
+        
+    def __len__(self):
+        return len(self.data['paths'])
+    
+
+    def __getitem__(self, idx):
+        
+        return sample
+
 
 class ClusterKinectDataset(Dataset):
     def __init__(self, transform, dataset_dir="datasets"):
         print("ClusterKinectDataset")
+
+        self.data_path = dataset_dir+"/ClusterKinectDataset"
+        
+
 
 class PoseKinectDataset(Dataset):
     def __init__(self, transform, dataset_dir="datasets"):
