bd670a0a626902bfe42f4a447833f03525494f8e
DavidC001 <davidecavicchini@hotmail.com>
2024-09-23 00:07:26 +0200

    feat: GELU activation function for pose estimation

--- pose_estimation/models.py
+++ pose_estimation/models.py
@@ -1,30 +1,33 @@
 import sys
+
 sys.path.append(".")
 
 from torch import nn
 import torch
 from contrastive_training.simclr.model import get_simclr_net
 from contrastive_training.simsiam.model import get_siam_net
-#from contrastive_training.MoCo.model import get_moco_net
-#from contrastive_training.supervised.model import get_supervised_net
+
+# from contrastive_training.MoCo.model import get_moco_net
+# from contrastive_training.supervised.model import get_supervised_net
 from torchvision.models import resnet50, resnet18
 from torchvision.models import ResNet50_Weights, ResNet18_Weights
 
 
 models = {
-    'simsiam': get_siam_net,
-    'simclr': get_simclr_net,
+    "simsiam": get_siam_net,
+    "simclr": get_simclr_net,
     #'MoCo': get_moco_net,
-    'LASCon': get_simclr_net,
-    'resnet': None
+    "LASCon": get_simclr_net,
+    "resnet": None,
 }
 
 
 class Linear(nn.Module):
     """
-    Linear layer with ReLU activation function for the pose estimation model.
+    Linear layer with GELU activation function for the pose estimation model.
     """
-    def __init__(self, layers, output_dim=48, base_model='resnet18', layer_norm=True):
+
+    def __init__(self, layers, output_dim=48, base_model="resnet18", layer_norm=True):
         """
         Initialize the Linear layer.
 
@@ -36,29 +39,39 @@ class Linear(nn.Module):
         """
         super(Linear, self).__init__()
         self.layers = nn.Sequential()
-        #attach 2048 to the beginning of the list
-        if base_model == 'resnet50':
+        # attach 2048 to the beginning of the list
+        if base_model == "resnet50":
             layers.insert(0, 2048)
-        elif base_model == 'resnet18':
+        elif base_model == "resnet18":
             layers.insert(0, 512)
         else:
             raise ValueError("Invalid base model")
         layers.append(output_dim)
         if layer_norm:
             self.LN = nn.LayerNorm(layers[0])
-        for i in range(len(layers)-1):
-            self.layers.add_module('linear'+str(i), nn.Linear(layers[i], layers[i+1]))
-            if i < len(layers)-2:
-                self.layers.add_module('relu'+str(i), nn.ReLU())
-        # breakpoint()
+        for i in range(len(layers) - 1):
+            self.layers.add_module(
+                "linear" + str(i), nn.Linear(layers[i], layers[i + 1])
+            )
+            if i < len(layers) - 2:
+                self.layers.add_module("gelu" + str(i), nn.GELU())
 
     def forward(self, x):
-        if hasattr(self, 'LN'):
+        if hasattr(self, "LN"):
             x = self.LN(x)
         z = self.layers(x)
         return z
 
-def getPoseEstimModel(path, model_type, layers, out_dim, device='cpu', base_model='resnet18', layer_norm=True):
+
+def getPoseEstimModel(
+    path,
+    model_type,
+    layers,
+    out_dim,
+    device="cpu",
+    base_model="resnet18",
+    layer_norm=True,
+):
     """
     Get the pose estimation model.
 
@@ -74,22 +87,22 @@ def getPoseEstimModel(path, model_type, layers, out_dim, device='cpu', base_mode
     Returns:
     - base: torch.nn.Module, pose estimation model with the specified weights
     """
-    if model_type != 'resnet':
+    if model_type != "resnet":
         base = models[model_type](base_model)
         base.load_state_dict(torch.load(path, map_location=torch.device(device)))
     else:
-        if base_model == 'resnet50':
+        if base_model == "resnet50":
             weights = ResNet50_Weights.DEFAULT
             base = nn.DataParallel(resnet50(weights=weights))
-        elif base_model == 'resnet18':
+        elif base_model == "resnet18":
             weights = ResNet18_Weights.DEFAULT
             base = nn.DataParallel(resnet18(weights=weights))
         else:
             raise ValueError("Invalid base model")
-    
+
     base = base.to(device)
-    
-    if model_type == 'simsiam' or model_type == 'MoCo':
+
+    if model_type == "simsiam" or model_type == "MoCo":
         base.module = base.module.base
     base.module.fc = Linear(layers, out_dim, base_model, layer_norm)
 
