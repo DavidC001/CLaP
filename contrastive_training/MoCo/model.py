fix: queue dimensions and loss computation

contrastive_training/MoCo/model.py
@@ -73,7 +73,7 @@ class MoCo(nn.Module):
             param_k.requires_grad = False  # not update by gradient
 
         # create the queue
-        self.register_buffer("queue", torch.randn(dim_out, K))
+        self.register_buffer("queue", torch.randn(K, dim_out))
         self.queue = nn.functional.normalize(self.queue, dim=0)
 
         self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
@@ -87,9 +87,9 @@ class MoCo(nn.Module):
             param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
 
     @torch.no_grad()
-    def _dequeue_and_enqueue(self, keys):
+    def _dequeue_and_enqueue(self, keys_batch):
         # gather keys before updating queue
-        keys = concat_all_gather(keys)
+        keys = concat_all_gather(keys_batch)
 
         batch_size = keys.shape[0]
 
@@ -97,13 +97,16 @@ class MoCo(nn.Module):
 
         # assert self.K % batch_size == 0  # for simplicity
 
-        # replace the keys at ptr (dequeue and enqueue)
-        if ptr + batch_size > self.K:
-            self.queue[:, ptr:] = keys[:self.K - ptr]
-            self.queue[:, :batch_size - (self.K - ptr)] = keys[self.K - ptr:]
-        else:
-            self.queue[:, ptr:ptr + batch_size] = keys
-        
+        try:
+            # replace the keys at ptr (dequeue and enqueue)
+            if ptr + batch_size > self.K:
+                self.queue[ptr:, :] = keys[:self.K - ptr]
+                self.queue[:batch_size - (self.K - ptr), :] = keys[self.K - ptr:]
+            else:
+                self.queue[ptr:ptr + batch_size, :] = keys
+        except Exception as e:
+            breakpoint()
+            
         ptr = (ptr + batch_size) % self.K  # move pointer
 
         self.queue_ptr[0] = ptr
@@ -189,7 +192,8 @@ class MoCo(nn.Module):
       keys = select_random_rows(embeddings_k)
 
       # dequeue and enqueue
-      self._dequeue_and_enqueue(keys)
+      if self.training:
+        self._dequeue_and_enqueue(keys)
 
       # Return embeddings
       return q, embeddings_k
@@ -201,8 +205,7 @@ def concat_all_gather(tensor):
     Performs all_gather operation on the provided tensors.
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
-    tensors_gather = [torch.ones_like(tensor)
-        for _ in range(torch.distributed.get_world_size())]
+    tensors_gather = [torch.ones_like(tensor) for _ in range(torch.distributed.get_world_size())]
     torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
 
     output = torch.cat(tensors_gather, dim=0)

contrastive_training/MoCo/train.py
@@ -10,6 +10,8 @@ import torch.backends.cudnn as cudnn
 from contrastive_training.MoCo.model import get_moco_net
 from contrastive_training.utils import get_datasetsMoco
 
+from tqdm.auto import tqdm
+
 from torch.utils.tensorboard import SummaryWriter
 
 random.seed(42)
@@ -33,9 +35,9 @@ def get_loss(q, k_plus, k_negatives, t_plus, t_negative, batch_size):
 
           sum_plus += exp_q_dot_k_plus
 
-      for i in range(k_negatives.size(1)):
+      for i in range(k_negatives.size(0)):
           # Calculate the dot products
-          q_dot_k_negatives = torch.dot(q[j], k_negatives[:, i]) / t_negative
+          q_dot_k_negatives = torch.dot(q[j], k_negatives[i,:]) / t_negative
 
           # Calculate the exponentials
           exp_q_dot_k_negatives = torch.exp(q_dot_k_negatives)
@@ -43,7 +45,7 @@ def get_loss(q, k_plus, k_negatives, t_plus, t_negative, batch_size):
           sum_negatives += exp_q_dot_k_negatives
 
       # Compute the loss
-      loss += -torch.log(sum_plus / (sum_negatives + 1e-10))  # avoid division by zero
+      loss += -torch.log(sum_plus / (sum_negatives + sum_plus + 1e-10))  # avoid division by zero
 
     return loss / batch_size
 
@@ -74,7 +76,7 @@ def train_step(train_loader, model, optimizer, epoch, device, batch_size):
     samples = 0.
     cumulative_loss = 0.
 
-    for i, images in enumerate(train_loader):
+    for i, images in tqdm(enumerate(train_loader), total=len(train_loader)):
 
         image_q = torch.tensor(images["query"]).to(device)
 
@@ -102,7 +104,7 @@ def train_step(train_loader, model, optimizer, epoch, device, batch_size):
         cumulative_loss += loss.item()
         samples += batch_size
 
-        return cumulative_loss / samples
+    return cumulative_loss / samples
 
 def val_step(val_loader, model, optimizer, epoch, device, batch_size):
 
@@ -114,7 +116,7 @@ def val_step(val_loader, model, optimizer, epoch, device, batch_size):
 
     with torch.no_grad():
         #end = time.time()
-        for i, images in enumerate(val_loader):
+        for i, images in tqdm(enumerate(val_loader), total=len(val_loader)):
             image_q = torch.tensor(images["query"]).to(device)
 
             images_k = []
@@ -136,7 +138,7 @@ def val_step(val_loader, model, optimizer, epoch, device, batch_size):
             cumulative_loss += loss.item()
             samples += batch_size
 
-            return cumulative_loss / samples
+    return cumulative_loss / samples
 
 
 def train_moco(model_dir, dataset_dir, datasets, save_every, batch_size=256, base_model='resnet18', name="moco", device='cuda', 

contrastive_training/utils.py
@@ -122,7 +122,7 @@ def get_datasetsMoco(datasets, batch_size, dataset_dir="datasets", base_model="r
 
     assert train_data is not None, "Dataset not loaded"
 
-    available_workers = math.ceil(os.cpu_count() / 2)
+    available_workers = 1
 
     train_loader = torch.utils.data.DataLoader(train_data, batch_size, shuffle=True, num_workers=available_workers)
     val_loader = torch.utils.data.DataLoader(val_data, batch_size, shuffle=False, num_workers=available_workers)